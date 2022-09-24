7f9fd79d4c3b6af4a8d4655a5e82b20a9f9a7241
DavidWaza <wazabanga00@gmail.com>
2022-09-24 15:04:44 +0100

    adjusted the hero image

--- components/Herobanner/index.js
+++ components/Herobanner/index.js
@@ -17,82 +17,88 @@ import DoodleFour from "../../public/img/doodle4.png";
 import DoodleFive from "../../public/img/doodle5.png";
 import DoodleSix from "../../public/img/doodle6.png";
 import BookBoy from "../../public/img/bookboy.png";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Herobanner = () => {
   return (
     <>
-      <div className="relative truncate">
-        <Image src={HerobannerBg} alt="heroImg" height={650} />
-        <h1
-          className={`${styles.bigText} absolute bottom-80 left-10 text-white text-7xl tracking-widest`}
-        >
-          Admission
-        </h1>
-        <h3
-          className={`${styles.subText} absolute text-white text-3xl tracking-wide`}
-        >
-          open for 2022 - 2023
-        </h3>
-        <p
-          className={`${styles.subTextSys} absolute text-white tracking-wider`}
-        >
-          Creche | Nursery | Grade
-        </p>
-        <Link href="/admission">
-          <a>
-            <button className={styles.enrolbuttonbk}>
-              <Image src={ButtonBack} alt="button" />
-            </button>
-            <button className={styles.enrolbuttonfk}>
-              <Image src={EnrollButton} alt="button" />
-              <span className="absolute top-0 left-10 text-white">
-                <p>Enroll</p>
-              </span>
-            </button>
-          </a>
-        </Link>
-        <div className={styles.transBoy}>
-          <Image src={Boy} alt="" />
-        </div>
-        <div className="absolute top-40 right-60">
-          <Image src={Ball1} alt="ball" />
-        </div>
-        <div className="absolute top-60 right-40">
-          <Image src={Ball2} alt="ball" />
-        </div>
-        <div className="absolute top-60 right-60">
-          <Image src={Ball3} alt="ball" />
-        </div>
-        <div className={styles.ballOne}>
-          <Image src={Ball4} alt="ball" />
-        </div>
-        <div className={styles.ballTwo}>
-          <Image src={Ball2} alt="ball" />
-        </div>
-        <div className={styles.ballThree}>
-          <Image src={Ball1} alt="ball" />
-        </div>
-        <div className={`${styles.doodleOne}`}>
-          <Image src={DoodleOne} alt="doodle" />
-        </div>
-        <div className={`${styles.doodleTwo}`}>
-          <Image src={DoodleTwo} alt="doodle" />
-        </div>
-        <div className={`${styles.doodleThree}`}>
-          <Image src={DoodleThree} alt="doodle" />
-        </div>
-        <div className={`${styles.doodleFour}`}>
-          <Image src={DoodleFour} alt="doodle" />
-        </div>
-        <div className={`${styles.doodleFive}`}>
-          <Image src={DoodleFive} alt="doodle" />
-        </div>
-        <div className={`${styles.doodleSix}`}>
-          <Image src={DoodleSix} alt="doodle" />
-        </div>
-        <div className={`${styles.bookBoy} absolute bottom-0 left-80`}>
-          <Image src={BookBoy} alt="readingboy" />
-        </div>
+      <div className={styles.Herobanner}>
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <div className={styles.leftContainer}>
+                <p className="text-white tracking-wide text-7xl font-bold">
+                  Admission
+                </p>
+                <p className="text-white text-xl font-bold tracking-wide">
+                  open for <span className="text-3xl">2022 - 2023</span>
+                </p>
+                <p className="text-white tracking-wide">
+                  Creche | Nursery | Grade School
+                </p>
+              </div>
+              <div>
+                <Link href="/">
+                  <a>
+                    <button className={styles.enrolbuttonbk}>
+                      <Image src={ButtonBack} alt="button" />
+                    </button>
+                    <button className={styles.enrolbuttonfk}>
+                      <Image src={EnrollButton} alt="button" />
+                      <span className="absolute top-0 left-10 text-white">
+                        <p>Enroll</p>
+                      </span>
+                    </button>
+                  </a>
+                </Link>
+              </div>
+            </Col>
+            <Col sm={8}>
+              <div className="hidden">
+                <div className={styles.transBoy}>
+                  <Image src={Boy} alt="boy" />
+                </div>
+                <div className="absolute top-32 right-60">
+                  <Image src={Ball1} alt="ball" />
+                </div>
+
+                <div className="absolute top-32 right-40">
+                  <Image src={Ball2} alt="ball" />
+                </div>
+                <div className="absolute top-52 right-60">
+                  <Image src={Ball3} alt="ball" />
+                </div>
+                <div className={styles.ballOne}>
+                  <Image src={Ball4} alt="ball" />
+                </div>
+                <div className={styles.ballTwo}>
+                  <Image src={Ball2} alt="ball" />
+                </div>
+                <div className={styles.ballThree}>
+                  <Image src={Ball1} alt="ball" />
+                </div>
+                <div className={`${styles.doodleOne}`}>
+                  <Image src={DoodleOne} alt="doodle" />
+                </div>
+                <div className={`${styles.doodleTwo}`}>
+                  <Image src={DoodleTwo} alt="doodle" />
+                </div>
+                <div className={`${styles.doodleThree}`}>
+                  <Image src={DoodleThree} alt="doodle" />
+                </div>
+                <div className={`${styles.doodleFour}`}>
+                  <Image src={DoodleFour} alt="doodle" />
+                </div>
+                <div className={`${styles.doodleFive}`}>
+                  <Image src={DoodleFive} alt="doodle" />
+                </div>
+                <div className={`${styles.doodleSix}`}>
+                  <Image src={DoodleSix} alt="doodle" />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
