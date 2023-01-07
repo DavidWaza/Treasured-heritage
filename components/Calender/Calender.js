3aa9bf1c3197af69e64a2067665907d289bb4b80
David Waza <wazabanga00@gmail.com>
2023-01-07 19:18:02 +0100

    designed the calender component

--- components/Calender/Calender.js
+++ components/Calender/Calender.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import styles from "../../styles/Calender.module.css"
+import styles from "../../styles/Calender.module.css";
 import {
   daysOfWeek,
   createDaysForCurrentMonth,
@@ -8,20 +8,20 @@ import {
   createDaysForPreviousMonth,
   isWeekendDay,
   getMonthDropdownOptions,
-  getYearDropdownOptions
+  getYearDropdownOptions,
 } from "./helpers";
 
 Calendar.propTypes = {
   className: PropTypes.string,
   yearAndMonth: PropTypes.arrayOf(PropTypes.number).isRequired, // e.g. [2021, 6] for June 2021
   onYearAndMonthChange: PropTypes.func.isRequired,
-  renderDay: PropTypes.func
+  renderDay: PropTypes.func,
 };
 export default function Calendar({
   className = "",
   yearAndMonth = [2021, 6],
   onYearAndMonthChange,
-  renderDay = () => null
+  renderDay = () => null,
 }) {
   const [year, month] = yearAndMonth;
 
@@ -35,7 +35,7 @@ export default function Calendar({
   let calendarGridDayObjects = [
     ...previousMonthDays,
     ...currentMonthDays,
-    ...nextMonthDays
+    ...nextMonthDays,
   ];
 
   const handleMonthNavBackButtonClick = () => {
@@ -74,8 +74,12 @@ export default function Calendar({
     <div className={styles.calendarRoot}>
       <div className={styles.navigationHeader}>
         <div className={styles.monthNavArrowButtons}>
-          <button onClick={handleMonthNavBackButtonClick}> prev </button>
-          <button onClick={handleMonthNavForwardButtonClick}>next</button>
+          <div className={styles.prevNavArrowButton}>
+            <button onClick={handleMonthNavBackButtonClick}> prev </button>
+          </div>
+          <div className={styles.nextNavArrowButton}>
+            <button onClick={handleMonthNavForwardButtonClick}>next</button>
+          </div>
         </div>
         <select
           className={styles.monthSelect}
@@ -105,7 +109,7 @@ export default function Calendar({
           <div
             key={day}
             className={classNames(`${styles.dayOfWeekHeaderCell}`, {
-              "weekendDay": [6, 0].includes(index)
+              weekendDay: [6, 0].includes(index),
             })}
           >
             {day}
@@ -117,8 +121,8 @@ export default function Calendar({
           <div
             key={day.dateString}
             className={classNames(`${styles.dayGridItemContainer}`, {
-              "weekendDay": isWeekendDay(day.dateString),
-              "currentMonth": day.isCurrentMonth
+              weekendDay: isWeekendDay(day.dateString),
+              currentMonth: day.isCurrentMonth,
             })}
           >
             <div className={styles.dayContentWrapper}>{renderDay(day)}</div>
@@ -130,7 +134,7 @@ export default function Calendar({
 }
 
 CalendarDayHeader.propTypes = {
-  calendarDayObject: PropTypes.object.isRequired
+  calendarDayObject: PropTypes.object.isRequired,
 };
 export function CalendarDayHeader({ calendarDayObject }) {
   return (
--- pages/calender.js
+++ pages/calender.js
@@ -1,22 +1,39 @@
 import React from "react";
+import Link from "next/link";
 import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../styles/Calender.module.css";
 import Calendar, { CalendarDayHeader } from "../components/Calender/Calender";
 
 const Calender = () => {
-  const [yearAndMonth, setYearAndMonth] = useState([2021, 9]);
+  const [yearAndMonth, setYearAndMonth] = useState([2023, 1, 2, 3]);
 
   return (
-    <div>
-      <Calendar
-        yearAndMonth={yearAndMonth}
-        onYearAndMonthChange={setYearAndMonth}
-        renderDay={(calendarDayObject) => (
-          <div>
-            <CalendarDayHeader calendarDayObject={calendarDayObject} />
-          </div>
-        )}
-      />
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <div className={styles.schText}>
+              <p>School Calender</p>
+            </div>
+            <div>
+              <Link href="#">
+                <p className={styles.downloadCalender}>2023 - 2024 calender</p>
+              </Link>
+            </div>
+            <Calendar
+              yearAndMonth={yearAndMonth}
+              onYearAndMonthChange={setYearAndMonth}
+              renderDay={(calendarDayObject) => (
+                <div>
+                  <CalendarDayHeader calendarDayObject={calendarDayObject} />
+                </div>
+              )}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
