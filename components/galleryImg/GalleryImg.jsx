2a646374c25a39bedb408fe4220c5f88c1b32d74
David Waza <wazabanga00@gmail.com>
2023-01-20 08:22:06 +0100

    made gallery image a reusable component

--- components/galleryImg/GalleryImg.jsx
+++ components/galleryImg/GalleryImg.jsx
@@ -6,20 +6,31 @@ import Link from "next/link";
 const GalleryImg = (props) => {
   return (
     <>
-      <div className={styles.coverImg}>
-        <Image
-          src={props.src}
-          alt={props.alt}
-          fill
-          objectPosition="center"
-          objectSize="cover"
-        />
-      </div>
-      <div className={styles.coverTitle}>
-        <Link href={props.link} as={props.as}>
-          <p>{props.title}</p>
-        </Link>
-      </div>
+      <Link href={props.link} as={props.as}>
+        <label className={styles.card}>
+          <div className={styles.cardBody}>
+            <div className={styles.cardBodyCover}>
+              <img
+                alt="img"
+                className={styles.cardBodyCoverImage}
+                src={props.src}
+              />
+              <span className={styles.cardBodyCoverCheckbox}>
+                <svg
+                  className={styles.cardBodyCoverCheckboxSvg}
+                  viewBox="0 0 12 10"
+                >
+                  <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                </svg>
+              </span>
+            </div>
+            <header className={styles.cardBodyHeader}>
+              <p className={styles.cardBodyHeaderSubtitle}>{props.category}</p>
+              <h2 className={styles.cardBodyHeaderTitle}>{props.title}</h2>
+            </header>
+          </div>
+        </label>
+      </Link>
     </>
   );
 };
--- pages/gallery.js
+++ pages/gallery.js
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 // import Image from "next/image";
 import { Col, Row, Container } from "react-bootstrap";
 import { GalleryImage } from "../components/DataArr/Data";
+import GalleryImg from "../components/galleryImg/GalleryImg";
 
 const GalleryAPI = () => {
   return (
@@ -20,31 +21,14 @@ const GalleryAPI = () => {
           <Row className="g-3">
             {GalleryImage.map((x) => (
               <Col sm={6} md={4} lg={4} key={x.id}>
-                <label className={styles.card}>
-                  <div className={styles.cardBody}>
-                    <div className={styles.cardBodyCover}>
-                      <img
-                        alt="img"
-                        className={styles.cardBodyCoverImage}
-                        src={x.src}
-                      />
-                      <span className={styles.cardBodyCoverCheckbox}>
-                        <svg
-                          className={styles.cardBodyCoverCheckboxSvg}
-                          viewBox="0 0 12 10"
-                        >
-                          <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                        </svg>
-                      </span>
-                    </div>
-                    <header className={styles.cardBodyHeader}>
-                      <p className={styles.cardBodyHeaderSubtitle}>
-                        {x.category}
-                      </p>
-                      <h2 className={styles.cardBodyHeaderTitle}>{x.title}</h2>
-                    </header>
-                  </div>
-                </label>
+                <GalleryImg
+                  src={x.src}
+                  alt="pic"
+                  category={x.category}
+                  title={x.title}
+                  link="/post/[id]"
+                  as={x.as}
+                />
               </Col>
             ))}
           </Row>
