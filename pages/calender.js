df03c6ea414388c39478c4bc85a0ced9f7f81328
David Waza <wazabanga00@gmail.com>
2023-01-26 09:56:26 +0100

    create an html downloadable button

--- pages/calender.js
+++ pages/calender.js
@@ -132,10 +132,11 @@ const Calender = () => {
               <h2>School Calender</h2>
               <Row>
                 <Col sm={6}>
-                  <button className={styles.calenderLink}>
+                  <button className={styles.calenderLink} type="submit">
                     <Link
-                      href="https://res.cloudinary.com/ddwkojhaj/image/upload/v1674575716/GRACE_TERM_NEWSLETTER-10_aeehpa.pdf"
+                      href="https://drive.google.com/file/d/1PDV_tvzCaoL0ttA7bK-CsmspgwuSh5b9/view?usp=share_link"
                       target="/blank"
+                      download="THS Calender"
                     >
                       <p className={styles.calLinkdownload}>
                         2023-2024 calender
@@ -146,8 +147,9 @@ const Calender = () => {
                 <Col sm={6}>
                   <button className={styles.newsletterLink}>
                     <Link
-                      href="file:///C:/Users/waza/Downloads/GRACE%20TERM%20NEWSLETTER-10.pdf"
+                      href="https://drive.google.com/file/d/1PDV_tvzCaoL0ttA7bK-CsmspgwuSh5b9/view?usp=share_link"
                       target="/blank"
+                      download="THS Newsletter"
                     >
                       <p className={styles.calLinkdownload}>
                         download Newsletter
