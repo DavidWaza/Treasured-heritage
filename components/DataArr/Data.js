8f804b19abc5e300ba6f5377df2d22585bb6fcef
David Waza <wazabanga00@gmail.com>
2023-01-28 21:40:22 +0100

    turn calender to pdf, created and inserted link

--- components/DataArr/Data.js
+++ components/DataArr/Data.js
@@ -94,7 +94,7 @@ export const TableHead = [
   },
   {
     id: 2,
-    title: "WEEK",
+    title: "WEEK(s)",
   },
   {
     id: 3,
--- pages/calender.js
+++ pages/calender.js
@@ -6,7 +6,7 @@ import { TableHead, TableData } from "../components/DataArr/Data";
 const Calender = () => {
   return (
     <>
-    <div className={styles.calenderBanner}></div>
+      <div className={styles.calenderBanner}></div>
       <Container>
         <Row>
           <Col>
@@ -17,7 +17,7 @@ const Calender = () => {
               <Col sm={6}>
                 <button className={styles.calenderLink} type="submit">
                   <Link
-                    href="https://drive.google.com/file/d/1PDV_tvzCaoL0ttA7bK-CsmspgwuSh5b9/view?usp=share_link"
+                    href="https://drive.google.com/file/d/1RZrx1OXWiPH-ubog4VqORVu0pdf-NWU5/view?usp=share_link"
                     target="/blank"
                     download="THS Calender"
                   >
@@ -39,17 +39,19 @@ const Calender = () => {
                 </button>
               </Col>
             </Row>
-            <Table striped bordered hover >
+            <Table striped bordered hover>
               <thead>
                 <tr>
                   {TableHead.map((x) => (
-                    <th key={x.id}>{x.title}</th>
+                    <th key={x.id} className={styles.tableStyles}>
+                      {x.title}
+                    </th>
                   ))}
                 </tr>
               </thead>
               <tbody>
                 {TableData.map((y) => (
-                  <tr key={y.id}>
+                  <tr key={y.id} className={styles.tableStyles}>
                     <td>{y.date}</td>
                     <td>{y.week}</td>
                     <td>{y.activity}</td>
